b0fe122c2bcd807bd9376afe5bdf807bef22b13b
madinaza <madinaza@code.edu.az>
2022-02-06 13:55:08 +0400

    eledim taskii

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FrontProject.DAL;
+using FrontProject.Models;
+using FrontProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            HomeVM model = new HomeVM
+            {
+                Sliders = _context.Sliders.OrderBy(s => s.Order).ToList(),
+                Carousels =_context.Carousels.Include(c=>c.Possition).Take(2).ToList()
+            };
+          
+            return View(model);
+        }
+
+    }
+}
---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FrontProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontProject.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+
+        public DbSet<Carousel> Carousels { get; set; }
+        public DbSet<Possition> Possitions { get; set; }
+
+
+    }
+}
